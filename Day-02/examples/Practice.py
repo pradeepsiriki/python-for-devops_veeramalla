5b1f154c78b6910fbd22c08727594b4aa6e0cd07
pradeepsiriki <35029907+pradeepsiriki@users.noreply.github.com>
2025-04-30 05:10:32 +0000

    add practice file

---
+++ Day-02/examples/Practice.py
@@ -0,0 +1,10 @@
+name = 'pradeep'
+print("lenght is" , len(name))
+uppername = name.upper()
+print("upper name is", uppername)
+# len upper string concat, replace, split, strip, substring, find, regex match
+rep = name.replace('deep', 'creep')
+print(rep)
+print(name.split('d')[0])
+
+print(name.find("deep"))
